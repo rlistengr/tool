f120e3dceedaa922ffe7c4b7336fa22bbb45c773
caozhenhua <caozhenhua@caozhenhuadeMacBook-Pro.local>
2019-12-17 15:20:53 +0800

    for backtrace

--- log.c
+++ log.c
@@ -8,6 +8,8 @@
 #include <signal.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <execinfo.h>
+#include <unistd.h>
 
 #define SERVER_IP   "127.0.0.1"
 #define BUF_SIZE    2000
@@ -42,9 +44,11 @@ void sendlog(unsigned short port, const char *fmt, ...)
     offset += vsnprintf(buf+offset, BUF_SIZE-offset, fmt, args);
     va_end(args);
     
-    if (offset >= BUF_SIZE) {
-        buf[BUF_SIZE-2] = '\n';
+    if ((offset+1) > (BUF_SIZE-1)) {
+        offset = BUF_SIZE-2;
     }
+    buf[offset] = '\n';
+    buf[offset+1] = 0;
 
     if (fd != -1) {
         struct sockaddr_in stAddr;
@@ -54,14 +58,14 @@ void sendlog(unsigned short port, const char *fmt, ...)
         stAddr.sin_port = htons(port);
         stAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-        sendto(fd, buf, offset, MSG_DONTWAIT, (struct sockaddr *)&stAddr, sizeof(stAddr));
+        sendto(fd, buf, offset+2, MSG_DONTWAIT, (struct sockaddr *)&stAddr, sizeof(stAddr));
     }
 
     return;
 }
 
 #define FILE_PATH  "/mnt/sdcard/log"
-static int write_log_mode = 0; //0- printf
+static int write_log_mode = 1; //0- printf
 
 void writelog(const char *fmt, ...)
 {
@@ -101,6 +105,7 @@ void writelog(const char *fmt, ...)
         va_start(args, fmt);
         vfprintf(fp, fmt, args);
         va_end(args);
+        fprintf(fp, "\n");
     } 
 
     return;
@@ -124,6 +129,7 @@ pthread_mutex_t log_lock = PTHREAD_MUTEX_INITIALIZER;
 void printCacheLog(int sig) {
     log_node_t *curr;
     
+    printf("* * * * * * * * * * * *\n");
     pthread_mutex_lock(&log_lock);
     curr = log_head;
     while (curr) {
@@ -170,9 +176,13 @@ void cachelog(const char *fmt, ...) {
     va_start(args, fmt);
     offset += vsnprintf(node->buf+offset, LOG_BUF_SIZE-offset, fmt, args);
     va_end(args);
-    if (offset >= LOG_BUF_SIZE) {
-        node->buf[LOG_BUF_SIZE-2] = '\n';
+    
+    if ((offset+1) > (LOG_BUF_SIZE-1)) {
+        offset = LOG_BUF_SIZE-2;
     }
+    node->buf[offset] = '\n';
+    node->buf[offset+1] = 0;
+    
     pthread_mutex_lock(&log_lock);
     if (log_count == 0) {
         log_head = node;
@@ -188,6 +198,33 @@ void cachelog(const char *fmt, ...) {
     return;
 }
 
+#define BT_BUF_SIZE 100
+//-rdynamic 使用gcc编译时需要带上这个选项
+void logBackTrace(void) {
+    int j, nptrs;
+    void *buffer[BT_BUF_SIZE];
+    char **strings;
+    
+    nptrs = backtrace(buffer, BT_BUF_SIZE);
+    //printf("backtrace() returned %d addresses\n", nptrs);
+
+    /* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+       would produce similar output to the following: */
+
+    strings = backtrace_symbols(buffer, nptrs);
+    if (strings == NULL) {
+        return;
+    }
+
+    for (j = 0; j < nptrs; j++) {
+        //cachelog(strings[j]);
+        //sendlog(0, strings[j]);
+        writelog(strings[j]);
+    }
+
+    free(strings);
+}
+
 int main() {
     int i = 1;
     char buf[4000];
@@ -196,7 +233,8 @@ int main() {
         memset(buf, (i%10)+48, i);
         buf[i+1] = 0;
 
-        writelog("%d--%s\n", i, buf);
+        //logBackTrace("%d--%s\n", i, buf);
+        logBackTrace();
         i++;
     }
 
